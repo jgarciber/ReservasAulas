552e896d2a4092b0de948574ac2a4a22518779d1
José Ramón Jiménez Reyes <joseramon.jimenez@iesalandalus.org>
2020-01-18 21:40:04 +0100

    Clase principal que orquesta el modelo, la vista y el controlador.

--- src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/MainApp.java
@@ -1,9 +1,16 @@
 package org.iesalandalus.programacion.reservasaulas;
 
+import org.iesalandalus.programacion.reservasaulas.mvc.controlador.Controlador;
+import org.iesalandalus.programacion.reservasaulas.mvc.modelo.Modelo;
+import org.iesalandalus.programacion.reservasaulas.mvc.vista.Vista;
+
 public class MainApp {
 
 	public static void main(String[] args) {
-		System.out.println("Gestión de las Reservas de Aulas del IES Al-Ándalus");
+		Modelo modelo = new Modelo();
+		Vista vista = new Vista();
+		Controlador controlador = new Controlador(modelo, vista);
+		controlador.comenzar();
 	}
 
 }
